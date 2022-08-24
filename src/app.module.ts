9c4df94bbf2bf80db06c6c162a9baa2e20239140
NikitaRubocki <nikita.rubocki@gmail.com>
2022-08-24 16:17:00 -0700

    Add coffee rating module to see dependencies in action
    Test out dependency injection and linking together different modules and services
    
    Create new coffee rating module that uses coffees services

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [CoffeesModule, CoffeeRatingModule],
   controllers: [AppController],
   providers: [AppService],
 })
