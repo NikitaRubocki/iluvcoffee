04cc592ec718ebe7d78092a88a715d90a55cd655
NikitaRubocki <nikita.rubocki@gmail.com>
2022-08-25 09:41:45 -0700

    Add config module to app
    Importing the config module allows easy manipulation of the configs variables for different environments, allowing us to change envs with a .env file instead of using hardcoded values. I didn't actually link up a postgres database but that would be useful here.
    
    Import the config nestjs package and add to the app module, helping us more easily manage configurations for different envs

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CoffeesModule, CoffeeRatingModule],
+  imports: [
+    ConfigModule.forRoot(),
+    CoffeesModule,
+    CoffeeRatingModule,
+    // database info would go here if we were using a database
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
